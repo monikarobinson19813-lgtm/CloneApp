a277424664a08857fa6f6513eb4c6fa04980762c
monikarobinson19813-lgtm <monikarobinson19813@gmail.com>
2026-09-19 16:58:49 +0530

    CA-1: use Espresso intents for picker contract

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,5 +23,6 @@ dependencies {
 
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test:runner:1.6.2")
+    androidTestImplementation("androidx.test.espresso:espresso-intents:3.6.1")
     androidTestImplementation("androidx.test.uiautomator:uiautomator:2.3.0")
 }
